f76d315066dfc36735d246de053f680bd16900a7
Abhishek Dasgupta <abhishek20dgp@gmail.com>
2022-10-16 12:46:18 +0530

    initial

---
+++ src/component/countdown/index.js
@@ -0,0 +1,19 @@
+import styles from './countdown.module.scss';
+import CountDownTimer from './countdowntimer.js';
+
+function Countdown() {
+
+    const hoursMinSecs = {hours:41, minutes: 11, seconds: 40}
+  
+    return (
+       
+      <div className={styles.App}>
+         <div className={styles.title}>Days to go...</div>
+         <div className={styles.timer}><CountDownTimer hoursMinSecs={hoursMinSecs}/>
+      </div>
+   </div>
+      
+    );
+  }
+
+export default Countdown;
