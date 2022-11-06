340197ce73d4d7cf866d3b25f5cfce1c6cf74104
Abhishek Dasgupta <abhishek20dgp@gmail.com>
2022-11-06 17:50:42 +0530

    Updated community partners

--- src/component/sponsors/index.js
+++ src/component/sponsors/index.js
@@ -3,6 +3,10 @@ import SponsorCard from "../../micro-component/SponsorCard/SponsorCard";
 import styles from "./sponsors.module.scss";
 import GMClogo from "../../assets/GMCLogo.png"
 import offseclogo from "../../assets/offsecLogo.jpg"
+import girlscriptlogo from "../../assets/girlscript.jpeg"
+import hack4bengal from "../../assets/hack4bengal.png"
+import ocd from  "../../assets/ocd.png"
+import devworld from  "../../assets/devworld.png"
 const Sponsors = () => {
   return (
     <div className={styles.background}>
@@ -33,6 +37,16 @@ const Sponsors = () => {
           <img src="https://resources.jetbrains.com/storage/products/company/brand/logos/jb_beam.png" alt="JetBrains Logo (Main) logo." className={styles.otherSponsors}/>
           <img src={GMClogo} alt="Offensive Security" className={styles.otherSponsors}/>
           <img src={offseclogo} alt="Offensive Security" className={styles.otherSponsors}/>
+          </div>
+      </div>
+      <div className={styles.others}>
+        <h1>Community Partners</h1>
+        <div className={styles.logos}>
+          <img src={girlscriptlogo} alt="Girlscript" className={styles.otherSponsors}/>
+          <img src="https://pbs.twimg.com/profile_images/1275851790991405056/7shBoO_T_400x400.jpg" alt="FlutterKolkata" className={styles.otherSponsors}/>
+          <img src={hack4bengal} alt="hack4bengal" className={styles.otherSponsors}/>
+          <img src={ocd} alt="ocd" className={styles.otherSponsors}/>
+          <img src={devworld} alt="devworld" className={styles.otherSponsors}/>
           </div>
       </div>
     </div>
